9aeab207bb695361f203498212cb09dab9a669d1
Martin Filliung <martinfilliung@gmail.com>
2026-04-27 23:17:59 +0900

    added terminal transparency and wallpaper refresh bind

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,3 +8,9 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Enable transparent background
+vim.o.termguicolors = true
+vim.cmd([[highlight Normal ctermbg=NONE guibg=NONE]])
+vim.cmd([[highlight NormalNC ctermbg=NONE guibg=NONE]])
+vim.cmd([[highlight NonText ctermbg=NONE guibg=NONE]])
