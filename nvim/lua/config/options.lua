a4e598f69aaabdcc19a202ac58eed09e915116ee
Martin Filliung <martin.filliung@aist.go.jp>
2026-04-30 11:31:17 +0900

    set whitespace

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,6 +8,17 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.list = true -- Show whitespace characters
+vim.opt.listchars:append({ space = "∙" }) -- Render spaces as a larger dot-like glyph
+
+local function set_whitespace_hl()
+  vim.api.nvim_set_hl(0, "Whitespace", { fg = "#acb0be", ctermfg = 245 })
+end
+
+set_whitespace_hl()
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = set_whitespace_hl,
+})
 
 -- Enable transparent background
 vim.o.termguicolors = true
