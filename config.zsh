13eba26557164b9df18339dc5ece38f39ced262e
Martin Filliung <martin.filliung@aist.go.jp>
2026-05-11 20:06:32 +0900

    cherry pick from work branck
    color sheme for any ssh connection
    
    added zsh config scripting
    
    added zsh config
    
    updated zsh config
    
    change timer format
    
    fix zsh config
    
    add devpod color scheme in kitty ssh
    
    added sccsh to copy select configs from host with scp then connect via
    ssh

---
+++ config.zsh
@@ -0,0 +1,51 @@
+# Prevent infinite loop when Oh My Zsh sources the custom directory
+if [[ "$__OMZ_RECURSION_GUARD" == "1" ]]; then
+  return
+fi
+
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
+sccsh(){
+    scp -r ~/.config/nvim "$1":~/.config/
+    scp -r ~/.config/lazygit "$1":~/.config/
+    ssh "$1"
+}
+
+alias cat="ccat"
+
+alias rhps1t="ssh rhps1user@rhps1t.s.aist.go.jp"
+alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
+alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
+
+# alias hrp5pt="ssh hrp5puser@hrp5pt.s.aist.go.jp"
+# alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
+# alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
+
+ZSH_THEME="bira"
+
+plugins=(
+  git
+  gitignore
+  gh
+  z
+  pip
+  pre-commit
+  ssh
+  ubuntu
+  timer
+  thefuck
+  dirhistory
+  command-not-found
+  colorize
+  aliases
+  zsh-autosuggestions
+)
+
+TIMER_FORMAT="%d"
+
+# Use a temporary guard so that manual 'source' from .zshrc still works
+if [[ -f "$ZSH/oh-my-zsh.sh" ]]; then
+  export __OMZ_RECURSION_GUARD=1
+  source "$ZSH/oh-my-zsh.sh"
+  unset __OMZ_RECURSION_GUARD
+fi
