c68f81eb7b6f35ca6ba57a6abcd1f6c7e408952f
Martin Filliung <martin.filliung@aist.go.jp>
2026-05-12 17:27:30 +0900

    updated zsh config

--- config.zsh
+++ config.zsh
@@ -1,52 +1,40 @@
-[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+# Prevent infinite loop if sourced by Oh My Zsh from custom directory
+if [[ -z "$CONFIG_ZSH_LOADED" ]]; then
+  export CONFIG_ZSH_LOADED=1
 
-alias cat="ccat"
+  [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
 
-alias rhps1t="ssh rhps1user@rhps1t.s.aist.go.jp"
-alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
-alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
+  alias cat="ccat"
 
-# alias hrp5pt="ssh hrp5puser@hrp5pt.s.aist.go.jp"
-# alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
-# alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
+  alias rhps1t="ssh rhps1user@rhps1t.s.aist.go.jp"
+  alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
+  alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
 
-eval $(thefuck --alias)
+  # alias hrp5pt="ssh hrp5puser@hrp5pt.s.aist.go.jp"
+  # alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
+  # alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
 
 ZSH_THEME="bira"
 
-plugins=(git gitignore gh)
-plugins+=(z ubuntu timer thefuck)
-plugins+=(dirhistory command-not-found colorize aliases)
+  plugins=(
+    git
+    gitignore
+    gh
+    z
+    ubuntu
+    timer
+    thefuck
+    dirhistory
+    command-not-found
+    colorize
+    aliases
+  )
 
+  # Only source OMZ if it hasn't been sourced yet
+  if [[ -f "$ZSH/oh-my-zsh.sh" ]]; then
+    source "$ZSH/oh-my-zsh.sh"
+  fi
+fi
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Content here will run even when sourced by OMZ
+# (though currently we have everything inside the guard)
