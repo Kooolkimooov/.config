6e7e98245bd0f5e571b74480be6a750fde565659
Martin Filliung <martin.filliung@aist.go.jp>
2026-05-15 11:38:03 +0900

    fix zsh config

--- config.zsh
+++ config.zsh
@@ -1,42 +1,45 @@
-# Prevent infinite loop if sourced by Oh My Zsh from custom directory
-if [[ -z "$CONFIG_ZSH_LOADED" ]]; then
-  export CONFIG_ZSH_LOADED=1
+# Prevent infinite loop when Oh My Zsh sources the custom directory
+if [[ "$__OMZ_RECURSION_GUARD" == "1" ]]; then
+  return
+fi
 
-  [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
 
-  alias cat="ccat"
+alias cat="ccat"
 
-  alias rhps1t="ssh rhps1user@rhps1t.s.aist.go.jp"
-  alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
-  alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
+alias rhps1t="ssh rhps1user@rhps1t.s.aist.go.jp"
+alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
+alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
 
-  # alias hrp5pt="ssh hrp5puser@hrp5pt.s.aist.go.jp"
-  # alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
-  # alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
+# alias hrp5pt="ssh hrp5puser@hrp5pt.s.aist.go.jp"
+# alias rhps1c="ssh rhps1user@rhps1c.s.aist.go.jp"
+# alias rhps1v="ssh rhps1user@rhps1v.s.aist.go.jp"
 
 ZSH_THEME="bira"
 
-  plugins=(
-    git
-    gitignore
-    gh
-    z
-    ubuntu
-    timer
-    thefuck
-    dirhistory
-    command-not-found
-    colorize
-    aliases
-  )
+plugins=(
+  git
+  gitignore
+  gh
+  z
+  pip
+  pre-commit
+  ssh
+  ubuntu
+  timer
+  thefuck
+  dirhistory
+  command-not-found
+  colorize
+  aliases
+  zsh-autosuggestions
+)
 
-  TIMER_FORMAT="%d"
+TIMER_FORMAT="%d"
 
-  # Only source OMZ if it hasn't been sourced yet
-  if [[ -f "$ZSH/oh-my-zsh.sh" ]]; then
-    source "$ZSH/oh-my-zsh.sh"
-  fi
+# Use a temporary guard so that manual 'source' from .zshrc still works
+if [[ -f "$ZSH/oh-my-zsh.sh" ]]; then
+  export __OMZ_RECURSION_GUARD=1
+  source "$ZSH/oh-my-zsh.sh"
+  unset __OMZ_RECURSION_GUARD
 fi
-
-# Content here will run even when sourced by OMZ
-# (though currently we have everything inside the guard)
